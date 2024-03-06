8a1d487dfd204e0139e6bdf791359935edbefa35
Brenden Ehlers <brenden@theehlers.org>
2024-03-06 15:50:17 -0700

    refactored loop.go

--- cache-node/domain/loop.go
+++ cache-node/domain/loop.go
@@ -1,7 +1,5 @@
 package domain
 
-import "fmt"
-
 func NewEventLoop(cache Cache) *EventLoop {
 	return &EventLoop{
 		cache: cache,
@@ -11,57 +9,60 @@ func NewEventLoop(cache Cache) *EventLoop {
 }
 
 func (eventLoop *EventLoop) Run() {
-	go func() {
-		for {
-			select {
-			case event := <-eventLoop.send:
-				switch event.Type {
-				case "get":
-					go eventLoop.getValue(event)
-				case "set":
-					go eventLoop.setValue(event)
-				case "delete":
-					go eventLoop.deleteValue(event)
-				default:
-					panic("Invalid event type")
-				}
-			case <-eventLoop.quit:
-				return
-			}
+	go eventLoop.startLoop()
+}
+
+func (eventLoop *EventLoop) startLoop() {
+	for {
+		select {
+		case event := <-eventLoop.send:
+			eventLoop.handleEvent(event)
+		case <-eventLoop.quit:
+			return
 		}
-	}()
+	}
+}
+
+func (eventLoop *EventLoop) handleEvent(event Event) {
+	switch event.Type {
+	case "get":
+		go eventLoop.getValue(event)
+	case "set":
+		go eventLoop.setValue(event)
+	case "delete":
+		go eventLoop.deleteValue(event)
+	default:
+		panic("Invalid event type")
+	}
 }
 
 func (eventLoop *EventLoop) getValue(event Event) {
-	fmt.Printf("Getting %s from the cache\n", event.Key)
-
 	val, ok := eventLoop.cache.Get(string(event.Key))
-
 	writeResponseToChannel(event.ResponseChan, ok, val)
 }
 
 func (eventLoop *EventLoop) setValue(event Event) {
-	fmt.Printf("Setting %s to %s in the cache\n", event.Key, event.Val)
-
-	err := eventLoop.cache.Set(event.Key, event.Val)
-	if err != nil {
-		event.ErrorChan <- err
-		return
+	if err := eventLoop.cache.Set(event.Key, event.Val); checkError(err) {
+		writeErrorToChannel(event.ErrorChan, err)
+	} else {
+		writeResponseToChannel(event.ResponseChan, true, nil)
 	}
-
-	writeResponseToChannel(event.ResponseChan, true, nil)
 }
 
 func (eventLoop *EventLoop) deleteValue(event Event) {
-	fmt.Printf("Deleting %s from the cache\n", event.Key)
-
-	err := eventLoop.cache.Delete(event.Key)
-	if err != nil {
-		event.ErrorChan <- err
-		return
+	if err := eventLoop.cache.Delete(event.Key); checkError(err) {
+		writeErrorToChannel(event.ErrorChan, err)
+	} else {
+		writeResponseToChannel(event.ResponseChan, true, nil)
 	}
+}
 
-	writeResponseToChannel(event.ResponseChan, true, nil)
+func checkError(err error) bool {
+	return err != nil
+}
+
+func writeErrorToChannel(channel chan error, err error) {
+	channel <- err
 }
 
 func writeResponseToChannel(channel chan EventResponse, ok bool, value CacheEntry) {
